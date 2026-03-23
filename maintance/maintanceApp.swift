fe5f594f73033bd6cbf55c41651dad024ad0f09b
Andrea Mercy <andreamercyd@gmail.com>
2026-03-23 21:58:02 +0530

    Initial Commit

---
+++ maintance/maintanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  maintanceApp.swift
+//  maintance
+//
+//  Created by Andrea Mercy on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct maintanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maintanceTests/maintanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  maintanceTests.swift
+//  maintanceTests
+//
+//  Created by Andrea Mercy on 23/03/26.
+//
+
+import Testing
+@testable import maintance
+
+struct maintanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
